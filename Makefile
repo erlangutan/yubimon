c194afb4f9409c5a4cea200a336729f1f406ce32
Cyrus Durgin <cyrus.durgin@oracle.com>
2016-08-18 11:30:54 -0700

    more or less working

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: yubimon
 
 yubimon: clean
-	gcc -L/usr/local/lib -I /usr/local/include/libusb-1.0 -lusb-1.0 -o bin/yubimon yubimon.c
+	gcc -Wall -L/usr/local/lib -I /usr/local/include/libusb-1.0 -lusb-1.0 -o bin/yubimon yubimon.c
 
 clean:
 	rm -f bin/yubimon
--- yubimon.c
+++ yubimon.c
@@ -1,32 +1,34 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <syslog.h>
+
 #include "libusb.h"
-#include <sys/ioctl.h>
-#include <fcntl.h>
-#include <unistd.h>
+
+int YUBIMON_RUNNING = 1;
 
 static int LIBUSB_CALL
-hotplug_callback_attach(libusb_context *ctx, libusb_device *dev, libusb_hotplug_event event, void *user_data) {
+hotplug_callback_attach(usb_ctx *ctx, usb_dev *dev, usb_event event, void *data) {
+    int rc = -1;
     struct libusb_device_descriptor desc;
 
-    if (LIBUSB_SUCCESS != libusb_get_device_descriptor(dev, &desc)) {
-        //fprintf(stderr, "Error getting device descriptor\n");
-        //need to bail!
+    if (LIBUSB_SUCCESS != (rc = libusb_get_device_descriptor(dev, &desc))) {
+        syslog(LOG_ERR, "Cannot get usb device descriptor: %s", libusb_error_name(rc));
         return EXIT_FAILURE;
     }
-    FILE * output = popen("ssh-add -s /Library/OpenSC/lib/opensc-pkcs11.so", "r");
-    pclose(output);
 
-    return 0;
+    return pclose(popen("ssh-add -s /Library/OpenSC/lib/opensc-pkcs11.so", "r"));
 }
 
 static int LIBUSB_CALL
-hotplug_callback_detach(libusb_context *ctx, libusb_device *dev, libusb_hotplug_event event, void *user_data)
+hotplug_callback_detach(usb_ctx *ctx, usb_dev *dev, usb_event event, void *data)
 {
-    FILE * output = popen("ssh-add -e /Library/OpenSC/lib/opensc-pkcs11.so", "r");
-    pclose(output);
+    return pclose(popen("ssh-add -e /Library/OpenSC/lib/opensc-pkcs11.so", "r"));
+}
 
-    return 0;
+void
+handle_sigterm(int signal) {
+    YUBIMON_RUNNING = 0;
 }
 
 int main(int argc, char *argv[])
@@ -40,51 +42,56 @@ int main(int argc, char *argv[])
     product_id = 0x0405;
     class_id   = LIBUSB_HOTPLUG_MATCH_ANY;
 
-    if (0 > (rc = libusb_init (NULL))) {
+    signal(SIGTERM, &handle_sigterm);
+    openlog("yubimon", LOG_PID, LOG_DAEMON);
+    syslog(LOG_NOTICE, "yubimon starting");
+
+    if (0 > (rc = libusb_init(NULL))) {
+        syslog(LOG_ERR, "Cannot initialize libusb: %s", libusb_error_name(rc));
         return EXIT_FAILURE;
     }
 
-    if (!libusb_has_capability (LIBUSB_CAP_HAS_HOTPLUG)) {
-        //printf ("Hotplug capabilites are not supported on this platform\n");
+    if (!libusb_has_capability(LIBUSB_CAP_HAS_HOTPLUG)) {
+        syslog(LOG_ERR, "No USB hotplug capability on this system");
         libusb_exit(NULL);
         return EXIT_FAILURE;
     }
 
-    if (LIBUSB_SUCCESS != libusb_hotplug_register_callback(NULL,
-                                                           LIBUSB_HOTPLUG_EVENT_DEVICE_ARRIVED,
-                                                           0,
-                                                           vendor_id,
-                                                           product_id,
-                                                           class_id,
-                                                           hotplug_callback_attach,
-                                                           NULL,
-                                                           &hp[0])) {
-        //fprintf(stderr, "Error registering attach callback\n");
+    if (LIBUSB_SUCCESS != (rc = libusb_hotplug_register_callback(NULL,
+                                                                 LIBUSB_HOTPLUG_EVENT_DEVICE_ARRIVED,
+                                                                 0,
+                                                                 vendor_id,
+                                                                 product_id,
+                                                                 class_id,
+                                                                 hotplug_callback_attach,
+                                                                 NULL,
+                                                                 &hp[0]))) {
+        syslog(LOG_ERR, "Cannot register hotplug attach callback: %s", libusb_error_name(rc));
         libusb_exit(NULL);
         return EXIT_FAILURE;
     }
 
-    if (LIBUSB_SUCCESS != libusb_hotplug_register_callback(NULL,
-                                                           LIBUSB_HOTPLUG_EVENT_DEVICE_LEFT,
-                                                           0,
-                                                           vendor_id,
-                                                           product_id,
-                                                           class_id,
-                                                           hotplug_callback_detach,
-                                                           NULL,
-                                                           &hp[1])) {
-        //fprintf(stderr, "Error registering detach callback\n");
+    if (LIBUSB_SUCCESS != (rc = libusb_hotplug_register_callback(NULL,
+                                                                 LIBUSB_HOTPLUG_EVENT_DEVICE_LEFT,
+                                                                 0,
+                                                                 vendor_id,
+                                                                 product_id,
+                                                                 class_id,
+                                                                 hotplug_callback_detach,
+                                                                 NULL,
+                                                                 &hp[1]))) {
+        syslog(LOG_ERR, "Cannot register hotplug detach callback: %s", libusb_error_name(rc));
         libusb_exit(NULL);
         return EXIT_FAILURE;
     }
 
-    int done = 0;
-    while (1) {
-        done += 1;
+    while (YUBIMON_RUNNING) {
         if (0 > (rc = libusb_handle_events(NULL))) {
-            printf("libusb_handle_events() failed: %s\n", libusb_error_name(rc));
+            syslog(LOG_ERR, "libusb_handle_events() failed: %s\n", libusb_error_name(rc));
         }
     }
+
+    syslog(LOG_NOTICE, "yubimon exiting");
 
     libusb_exit(NULL);
 
